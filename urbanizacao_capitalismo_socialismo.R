b648ee0e7d5a874fb0c97f12b8dd7db658691ba3
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-13 23:22:39 -0300

    Add files via upload

--- urbanizacao_capitalismo_socialismo.R
+++ urbanizacao_capitalismo_socialismo.R
@@ -6,8 +6,18 @@
 
 # Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
 
-### 
+### Mais da metade da população mundial agora vive em áreas urbanas - cada vez mais em cidades
+### densas. Os cenários urbanos são um fenômeno relativamente novo na história da humanidade.
+### Essa transição tem transformado a forma que vivemos, trabalhamos, viajamos e construimos
+### interações.
 
+### Nesse registro apresentamos uma visão geral da urbanização no mundo, estendendo-se 
+### do passado distante, até ao presente, e projecções de tendências futuras.
 
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
 
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
+library(ggthemes)
 
